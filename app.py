1b0a540c22bbb2f0fd3effef2913c7264e0c815a
Rajkishore2904 <61588407+Rajkishore2904@users.noreply.github.com>
2022-11-30 18:41:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ if (num % 2 == 0):
          st.write("The number is even")
 else:
          st.write("The number is odd")
-st.write("Assignment by Rajkishore Nandi")
-st.write("Rollno- 22f1006016")
+st.write("Assignment by - Rajkishore Nandi")
+st.write("Rollno - 22f1006016")
